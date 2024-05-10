31ff3ae93524c5156d32ef1e46699a83e2e1dd88
RBK <horatiohawk@yahoo.com>
2024-05-10 14:34:39 -0400

    Added configs for Azure models

--- sandbox/litellm/demo-test.sh
+++ sandbox/litellm/demo-test.sh
@@ -2,8 +2,8 @@
 # Script to demonstrate basic LLM forwarding for the models we currently have API keys and configs for.
 # Internal testing only
 
-MODELS=("gpt-3.5-turbo" "gpt-4" "gpt-4-turbo" "claude-3-opus" "claude-3-sonnet" "claude-3-haiku" "gemini-pro" "gemini-1.5-pro")
-EMB_MODELS=("text-embedding-3-large" "text-embedding-3-small")
+MODELS=("gpt-3.5-turbo" "gpt-4" "gpt-4-turbo" "claude-3-opus" "claude-3-sonnet" "claude-3-haiku" "gemini-pro" "gemini-1.5-pro" "azure-gpt-3.5-turbo-0613")
+#EMB_MODELS=("text-embedding-3-large" "text-embedding-3-small")
 
 for model in ${MODELS[@]}; do
   echo "Output for $model:"
@@ -19,7 +19,7 @@ for model in ${MODELS[@]}; do
           "content": "What is the result of adding two and two together?"
         }
       ]
-    }' | jq '.choices[0].message.content, .model'
+    }' #| jq '.choices[0].message.content, .model'
 
 done
 
